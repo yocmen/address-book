7a3861f6a4f3b32aa188373bf5d7f9542b452ffe
Yocsel Mendoza <yocmen@gmail.com>
2021-02-20 17:23:43 +0000

    Add home page, cremove example files from create-react-app

--- src/Routes/manager.js
+++ src/Routes/manager.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import App from "../App";
+import HomePage from "../Pages/home";
 
 const RoutesManager = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
-          <App />
-        </Route>
+        <Route exact path="/" component={HomePage} />
       </Switch>
     </Router>
   );

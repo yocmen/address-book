693b09d9e19ea6d1cbac5b3b4b340161b937e2f9
Yocsel Mendoza <yocmen@gmail.com>
2021-02-21 17:30:26 +0000

    Add no results message styling, add icons to messages

--- src/Components/NoResultsMessage.jsx
+++ src/Components/NoResultsMessage.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { FaExclamationCircle } from 'react-icons/fa';
 
 const NoResultsMessage = () => {
-  return <div role="alert">No users yet, please try again later.</div>;
+  return (
+    <div
+      role="alert"
+      className="flex items-center justify center space-x-2 border border-blue-300 bg-blue-200 px-4 py-2 rounded"
+    >
+      <FaExclamationCircle className="text-blue-700" />
+      <span className="text-blue-600">
+        No users yet, please try again later.
+      </span>
+    </div>
+  );
 };
 
 export default NoResultsMessage;
---
+++ src/Stories/NoResultsMessage.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NoResultsMessage from '../Components/NoResultsMessage';
+
+export default {
+  title: 'No results Message',
+  component: NoResultsMessage,
+};
+
+export const Default = () => {
+  return <NoResultsMessage />;
+};

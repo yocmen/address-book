b1369a02da90083054539ce3d9cbdf07e298372f
Yocsel Mendoza <yocmen@gmail.com>
2021-02-23 21:13:21 +0000

    Add husky + pre-push
    update run test command

--- src/Tests/usersListItem.test.jsx
+++ src/Tests/usersListItem.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import UsersListItem from '../Components/UsersListItem';
+import UsersListItem from '../Components/UsersList/Item';
 import generateUsers from './Factories/Users';
 
 describe('User list item', () => {
